82fa2910f8ca93f13625e27da37d4b14f4189336
mamau <mopckou_pak48rus@mail.ru>
2015-03-23 16:57:22 +0300

    cr8 robokassa

---
+++ app/Http/Controllers/PayController.php
@@ -0,0 +1,43 @@
+<?php namespace App\Http\Controllers;
+
+use Auth;
+use Request;
+use Session;
+use Redirect;
+
+class PayController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller renders your application's "dashboard" for users that
+	| are authenticated. Of course, you are free to change or remove the
+	| controller as you wish. It is just here to get your app started!
+	|
+	*/
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+	/**
+	 * Show the application dashboard to the user.
+	 *
+	 * @return Response
+	 */
+	public function getIndex()
+	{
+		$users = Auth::user();
+
+	}
+
+
+}
