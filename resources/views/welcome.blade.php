8b257aaad2b2b5df0a00419c57caa6de8a84a481
Александр <a.r.t.1.s.t@yandex.ru>
2015-05-22 01:09:15 +0300

    Изменения в контроллерах

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -240,7 +240,6 @@
 
 <div class="container">
     <!-- Example row of columns -->
-    <div class="row">
         <div class="col-md-4">
             <h2>WordPress</h2>
 
@@ -265,6 +264,57 @@
 
             <p><a role="button" href="#" class="btn btn-default">View details »</a></p>
         </div>
+
+
+    <div class="col-md-4">
+        <h2>WordPress</h2>
+
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+           amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
+        <p><a role="button" href="#" class="btn btn-default">View details »</a></p>
+    </div>
+    <div class="col-md-4">
+        <h2>Bitrix</h2>
+
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+           amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
+        <p><a role="button" href="#" class="btn btn-default">View details »</a></p>
+    </div>
+    <div class="col-md-4">
+        <h2>Joomla</h2>
+
+        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac
+           cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+
+        <p><a role="button" href="#" class="btn btn-default">View details »</a></p>
+    </div>
+
+
+    <div class="col-md-4">
+        <h2>WordPress</h2>
+
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+           amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
+        <p><a role="button" href="#" class="btn btn-default">View details »</a></p>
+    </div>
+    <div class="col-md-4">
+        <h2>Bitrix</h2>
+
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+           amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
+        <p><a role="button" href="#" class="btn btn-default">View details »</a></p>
+    </div>
+    <div class="col-md-4">
+        <h2>Joomla</h2>
+
+        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac
+           cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+
+        <p><a role="button" href="#" class="btn btn-default">View details »</a></p>
     </div>
 
     <hr>
