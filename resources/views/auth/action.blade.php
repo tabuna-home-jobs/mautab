2890100f8665bb49433a92649486f0c58fd40b11
tabuna <a.r.t.1.s.t@yandex.ru>
2015-06-05 10:07:27 +0300

    Что не работает, но позде подправим

--- resources/views/auth/action.blade.php
+++ resources/views/auth/action.blade.php
@@ -1,55 +1,6 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="/favicon.png"/>
-
-    <title>Cloudme</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,700,700italic&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="/animate.css">
-    <link rel="stylesheet" href="/style.css">
-    <script src="wow.js"></script>
-    <script>
-        new WOW().init();
-    </script>
-
-</head>
-<body>
+@include('header')
 
 
-<header class="header-login-app">
-
-    <nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Меню</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/">
-                    <img title="" alt="" src="/img/logo.png">
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="/auth/login">Войти</a></li>
-                    <li><a href="/auth/register">Зарегистрироваться</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-
-</header>
 
 
 <div class="container text-center">
--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,59 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/html">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" href="/favicon.png" />
-  <title>Cloudme</title>
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/font-awesome.min.css">
-  <link rel="stylesheet" href="/animate.css">
-  <link rel="stylesheet" href="/style.css">
-  <script src="/wow.js"></script>
-  <script>
-    new WOW().init();
-  </script>
-
-</head>
-<body>
+@include('header')
 
 
-<header class="header-login-bg">
-
-  <nav class="navbar navbar-default">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-          <span class="sr-only">Меню</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="/">
-          <img title="" alt="" src="/img/logo.png">
-        </a>
-      </div>
-
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav navbar-right">
-        <ul class="nav navbar-nav navbar-right">
-                  @if (Auth::guest())
-            <li><a href="/auth/login">Войти</a></li>
-            <li><a href="/auth/register">Зарегистрироваться</a></li>
-           @else
-          <li><a href="#">{{ Auth::user()->name }} </a></li>
-          @endif
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-
-
-
-  <div class="container text-center">
+<div class="container text-center">
     <h3>Хостинг для вашего сайта</h3>
 
     <h1>Регистрация</h1>
