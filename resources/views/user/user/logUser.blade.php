01666f24ae87bbf83e900d9781a475537893269d
tabuna <bliz48rus@gmail.com>
2015-08-13 20:18:15 +0300

    Обновил

--- resources/views/user/user/logUser.blade.php
+++ resources/views/user/user/logUser.blade.php
@@ -4,16 +4,68 @@
 
 
 
+
+
+
+
+
+    <div class="panel panel-default">
+        <div class="panel-heading">Basic form</div>
+        <div class="panel-body">
+
+
+
+
+
+
+
+
+            <ul class="timeline">
+
+                <li class="tl-header">
+                    <div class="btn btn-icon btn-rounded btn-default"><i class="fa fa-history"></i></div>
+                </li>
+
     @forelse($log as $elementlog)
-        <div class="col-xs-12 log-row">
-            <h1 class="col-md-3 col-xs-12 subheaderlog"> {{ $elementlog['DATE']  }} : {{ $elementlog['TIME']  }}</h1>
-            <blockquote class="col-md-9 col-xs-12">
-                <p>{{$elementlog['CMD']}}</p>
-            </blockquote>
-        </div>
-        <hr class="col-xs-12 row">
+
+        <li class="tl-item">
+            <div class="tl-wrap b-primary">
+                <span class="tl-date">{{$elementlog['TIME']}}</span>
+                <div class="tl-content panel padder b-a block">
+                    <span class="arrow left pull-up hidden-left"></span>
+                    <span class="arrow right pull-up visible-left"></span>
+                    <div class="text-lt">{{$elementlog['CMD']}}</div>
+                    <small class="text-right">{{$elementlog['DATE']}}</small>
+                </div>
+            </div>
+        </li>
+
+
     @empty
-        <div class="col-xs-12 text-center"><h2>Лог чист</h2></div>
+
+                    <li class="tl-item">
+                        <div class="tl-wrap b-primary">
+                            <span class="tl-date"></span>
+                            <div class="tl-content panel padder b-a block">
+                                <span class="arrow left pull-up hidden-left"></span>
+                                <span class="arrow right pull-up visible-left"></span>
+                                <div class="text-lt">Лог чист</div>
+                            </div>
+                        </div>
+                    </li>
     @endforelse
+
+
+                <li class="tl-header">
+                    <div class="btn btn-icon btn-rounded btn-default"><i class="fa fa-history"></i></div>
+                </li>
+
+            </ul>
+
+
+
+        </div>
+    </div>
+
 
 @endsection
