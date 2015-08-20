6a0c19a7819c7e6b7f8c6a4a969d8ad786ca2f93
tabuna <bliz48rus@gmail.com>
2015-08-21 00:30:11 +0300

    Телефон

--- resources/views/user/user/settings.blade.php
+++ resources/views/user/user/settings.blade.php
@@ -133,6 +133,14 @@
 
 
                                         <div class="form-group">
+                                            <label class="col-sm-3 control-label">Телефон</label>
+
+                                            <div class="col-sm-9"><input type="text" class="form-control"
+                                                                         value=""></div>
+                                        </div>
+
+
+                                        <div class="form-group">
                                             <label class="col-sm-3 control-label" for="email">Электронная почта</label>
 
                                             <div class="col-sm-9"><input type="email" name="email" class="form-control"
@@ -250,6 +258,11 @@
                                             </label>
                                         </div>
 
+                                        <div class="form-group">
+                                            <div class="col-sm-offset-3 col-sm-9">
+                                                <button type="submit" class="btn btn-default">Изменить</button>
+                                            </div>
+                                        </div>
 
                                     </div>
 
