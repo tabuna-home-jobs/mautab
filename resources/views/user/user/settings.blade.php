e5ee617fb79d8d3e7d976db01ab40f42bc5c3881
tabuna <bliz48rus@gmail.com>
2015-08-20 23:08:52 +0300

    Исправления в настройках

--- resources/views/user/user/settings.blade.php
+++ resources/views/user/user/settings.blade.php
@@ -12,7 +12,87 @@
                 <div class="row">
 
 
-                    <form class="col-md-12" action="{{URL::route('settings.update')}}" method="post">
+                    <div class="col-md-4">
+
+
+                        <div class="col wrapper-lg bg-light dk r-r">
+                            <h4 class="font-thin m-t-none m-b"><i class="fa fa-info-circle"></i> Информация</h4>
+
+
+                            <p class="m-b-md">Статус :
+                                @if($UserInfo['SUSPENDED'] == "no")
+                                    <span class="label label-success pull-right">Активен</span>
+                                @else
+                                    <span class="label label-danger pull-right">Не активен</span>
+                                @endif
+                            </p>
+
+
+                            <div class="">
+                                <div class="m-b">
+                                    <p class="pull-right text-primary">{{$UserInfo['U_BANDWIDTH']}}
+                                        из {{$UserInfo['BANDWIDTH']}}</p>
+                                    <span>{{Lang::get('menu.traffic')}}</span>
+                                </div>
+                                <div class="progress progress-xs">
+                                    <div class="progress-bar progress-bar-primary"
+                                         style="width: {{$UserInfo['U_BANDWIDTH']/$UserInfo['BANDWIDTH'] * 100  }}%"></div>
+                                </div>
+
+                                <div class="m-b">
+                                    <span class="pull-right text-info">{{$UserInfo['U_DISK']}}
+                                        из {{$UserInfo['DISK_QUOTA']}}</span>
+                                    <span>{{Lang::get('menu.disk')}}</span>
+                                </div>
+                                <div class="progress progress-xs">
+                                    <div class="progress-bar progress-bar-info"
+                                         style="width: {{$UserInfo['U_DISK']/$UserInfo['DISK_QUOTA'] * 100  }}%"></div>
+                                </div>
+
+
+                                <div class="m-b">
+                            <span class="pull-right text-info">{{ $UserInfo['U_WEB_DOMAINS'] }}
+                                из {{ $UserInfo['WEB_DOMAINS'] }}</span>
+                                    <span>{{Lang::get('menu.domains')}}</span>
+                                </div>
+                                <div class="progress progress-xs">
+                                    <div class="progress-bar progress-bar-info"
+                                         style="width: {{$UserInfo['U_WEB_DOMAINS']/$UserInfo['WEB_DOMAINS'] * 100  }}%"></div>
+                                </div>
+
+
+                                <div class="m-b">
+                            <span class="pull-right text-info">{{ $UserInfo['U_DNS_DOMAINS'] }}
+                                из {{ $UserInfo['DNS_DOMAINS'] }}</span>
+                                    <span>{{Lang::get('menu.DNS')}}</span>
+                                </div>
+                                <div class="progress progress-xs">
+                                    <div class="progress-bar progress-bar-info"
+                                         style="width: {{$UserInfo['U_DNS_DOMAINS']/$UserInfo['DNS_DOMAINS'] * 100  }}%"></div>
+                                </div>
+
+
+                                <div class="m-b">
+                            <span class="pull-right text-info">{{ $UserInfo['U_DATABASES'] }}
+                                из {{ $UserInfo['DATABASES'] }}</span>
+                                    <span>{{Lang::get('menu.BD')}}</span>
+                                </div>
+                                <div class="progress progress-xs">
+                                    <div class="progress-bar progress-bar-info"
+                                         style="width: {{$UserInfo['U_DATABASES']/$UserInfo['DATABASES'] * 100  }}%"></div>
+                                </div>
+
+                            </div>
+
+
+                            <p class="text-muted">Статистические данные обновляються каждые несколько часов</p>
+                        </div>
+
+
+                    </div>
+
+
+                    <form class="col-md-8" action="{{URL::route('settings.update')}}" method="post">
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Аккаунт</label>
@@ -59,7 +139,7 @@
 
                             <input type="hidden" name="_method" value="PUT">
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <input type="submit" value="Изменить" class="btn btn btn-blue">
+                            <input type="submit" value="Изменить" class="btn btn-primary">
 
                         </div>
 
