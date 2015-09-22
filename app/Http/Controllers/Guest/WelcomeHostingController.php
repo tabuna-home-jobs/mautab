5d3e65562c93f91f5533f1162be11a0c6ae64a9f
tabuna <bliz48rus@gmail.com>
2015-09-23 00:24:25 +0300

    Правки

--- app/Http/Controllers/Guest/WelcomeHostingController.php
+++ app/Http/Controllers/Guest/WelcomeHostingController.php
@@ -3,7 +3,6 @@
 use Mautab\Http\Controllers\Controller;
 use Mautab\Models\News;
 use Mautab\Models\Package;
-use SSH;
 
 class WelcomeHostingController extends Controller
 {
@@ -28,71 +27,6 @@ class WelcomeHostingController extends Controller
      */
     public function index()
     {
-
-        /*
-         * Drupal
-          SSH::into('testVDS')->run([
-              'cd /home/admin/web/test.mautab.com/public_html', //Тут надо брать путь
-              'rm -rf ./*',
-              'wget https://github.com/drupal/drupal/archive/7.x.zip',
-              'unzip 7.x.zip',
-              'rm  7.x.zip',
-              'mv drupal-7.x/* /home/admin/web/test.mautab.com/public_html',
-              'rm -rf drupal-7.x',
-          ]);
-        */
-
-        /*
-         * Joomla
-        SSH::into('testVDS')->run([
-            'cd /home/admin/web/test.mautab.com/public_html', //Тут надо брать путь
-            'rm -rf ./*',
-            'wget https://github.com/joomla/joomla-cms/archive/master.zip',
-            'unzip master.zip',
-            'rm  master.zip',
-            'mv joomla-cms-master/* /home/admin/web/test.mautab.com/public_html',
-            'rm -rf joomla-cms-master',
-        ]);
-
-          */
-
-
-        /*
-         * OpenCart
-        SSH::into('testVDS')->run([
-            'cd /home/admin/web/test.mautab.com/public_html', //Тут надо брать путь
-            'rm -rf ./*',
-            'wget https://github.com/opencart/opencart/archive/master.zip',
-            'unzip master.zip',
-            'rm  master.zip',
-            'mv opencart-master/upload/* /home/admin/web/test.mautab.com/public_html',
-            'rm -rf opencart-master',
-        ]);
-        */
-
-
-        /*
-        * WordPress
-        SSH::into('testVDS')->run([
-            'cd /home/admin/web/test.mautab.com/public_html', //Тут надо брать путь
-            'rm -rf ./*',
-            'wget https://github.com/WordPress/WordPress/archive/master.zip',
-            'unzip master.zip',
-            'rm  master.zip',
-            'mv WordPress-master/* /home/admin/web/test.mautab.com/public_html',
-            'rmdir WordPress-master',
-        ]);
-        */
-
-
-        /*
-                $date = Carbon::now()->addMinutes(2);
-                Queue::later($date, new TestJob());
-                dd($date, Carbon::now());
-        */
-        // Queue::pushOn('test', new TestJob());
-
-
         $Package = Package::whereHidden('false')->orderBy('price', 'ASC')->get();
         $News = News::paginate(3);
         return view('welcome.hosting', [
--- app/Jobs/CMS/InstallCMSJob.php
+++ app/Jobs/CMS/InstallCMSJob.php
@@ -2,11 +2,13 @@
 
 namespace Mautab\CMS\Jobs;
 
+use Config;
 use Illuminate\Contracts\Bus\SelfHandling;
 use Mautab\Jobs\Job;
 use Mautab\Models\CMS;
 use Mautab\Models\User;
 use SSH;
+
 
 class InstallCMSJob extends Job implements SelfHandling
 {
@@ -39,6 +41,14 @@ class InstallCMSJob extends Job implements SelfHandling
         $this->user = $user;
         $this->path = $path;
         $this->cms = $cms;
+
+        Config::set('remote.connections.Dynamic', [
+            'host' => Config::get('vesta.server')[$this->user->server]['ip'],
+            'username' => $this->user->nickname,
+            'password' => $this->user->password,
+        ]);
+
+
     }
 
     /**
@@ -48,11 +58,63 @@ class InstallCMSJob extends Job implements SelfHandling
      */
     public function handle()
     {
-        //Пример установки cms, у меня заработал хорошо, но на сервере не проверял
-        SSH::into($this->user->server)->run([
-            'cd /home/tabuna/web/test.mautab.com/public_html', //Тут надо брать путь
-            'wget ' . $this->cms->last_version,
+
+        /*
+         * Joomla
+         *
+         */
+        SSH::into('Dynamic')->run([
+            'cd /home/art1st/web/ok.com/public_html', //Тут надо брать путь
+            'rm -rf ./*',
+            'wget https://github.com/joomla/joomla-cms/archive/master.zip',
             'unzip master.zip',
+            'rm  master.zip',
+            'mv joomla-cms-master/* /home/art1st/web/ok.com/public_html',
+            'rm -rf joomla-cms-master',
         ]);
+
+
+        /*
+         * OpenCart
+        SSH::into('testVDS')->run([
+            'cd /home/admin/web/test.mautab.com/public_html',
+            'rm -rf ./*',
+            'wget https://github.com/opencart/opencart/archive/master.zip',
+            'unzip master.zip',
+            'rm  master.zip',
+            'mv opencart-master/upload/* /home/admin/web/test.mautab.com/public_html',
+            'rm -rf opencart-master',
+        ]);
+        */
+
+
+        /*
+        * WordPress
+        SSH::into('testVDS')->run([
+            'cd /home/admin/web/test.mautab.com/public_html', //Тут надо брать путь
+            'rm -rf ./*',
+            'wget https://github.com/WordPress/WordPress/archive/master.zip',
+            'unzip master.zip',
+            'rm  master.zip',
+            'mv WordPress-master/* /home/admin/web/test.mautab.com/public_html',
+            'rmdir WordPress-master',
+        ]);
+        */
+
+
+        /*
+         * Drupal надо потестить
+         *
+          SSH::into('Dynamic')->run([
+              'cd /home/art1st/web/habr.ru/public_html', //Тут надо брать путь
+              'rm -rf ./*',
+              'wget https://github.com/drupal/drupal/archive/7.x.zip',
+              'unzip 7.x.zip',
+              'rm  7.x.zip',
+              'mv drupal-7.x/* /home/art1st/web/habr.ru/public_html',
+              'rm -rf drupal-7.x',
+          ]);
+          */
+
     }
 }
