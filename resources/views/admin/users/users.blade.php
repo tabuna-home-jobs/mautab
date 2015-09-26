d349ad8361b941dcef777d6dda5ccc9191fe7ad7
tabuna <bliz48rus@gmail.com>
2015-09-27 01:54:47 +0300

    html fix

--- resources/views/admin/users/users.blade.php
+++ resources/views/admin/users/users.blade.php
@@ -2,12 +2,7 @@
 
 @section('content')
 
-
-
-
     <div class="app-content-body">
-
-
         <div class="bg-light lter b-b wrapper-md">
         <h1 class="m-n font-thin h3">Пользователи</h1>
     </div>
