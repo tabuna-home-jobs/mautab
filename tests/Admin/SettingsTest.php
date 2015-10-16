ea868f8145f20271c38570a0a71c1fcb452c17ad
tabuna <bliz48rus@gmail.com>
2015-10-16 16:41:58 +0300

    Ёбанный юнит тест

--- tests/Admin/SettingsTest.php
+++ tests/Admin/SettingsTest.php
@@ -10,14 +10,15 @@ class SettingsTest extends TestCase
     public function testIndex()
     {
         $response = $this->action('GET', 'Admin\SettingsController@index');
-        //$view = $response->original;
         $this->assertViewHas('Roles');
     }
 
 
     public function testStore()
     {
-
+        $response = $this->action('POST', 'Admin\SettingsController@store', [
+            ''
+        ]);
     }
 
 
