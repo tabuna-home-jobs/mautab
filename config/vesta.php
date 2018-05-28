df5429dbb5752a64fe5d97d24152d018b8abc360
Alexandr Chernyaev <bliz48rus@gmail.com>
2018-05-29 00:06:25 +0300

    Update vesta.php

--- config/vesta.php
+++ config/vesta.php
@@ -5,11 +5,11 @@ return [
 
     'server' => [
         'testVDS' => [
-            'ip' => '193.111.141.198',
-            'login' => 'admin',
-            'password' => '03af4dE1',
-            'ssh' => '03af4dE1',
-            'keyAPI' => 'VrakEMSQV226ba7p0e09yWOlnDVLkSpX'
+            'ip' => '',
+            'login' => '',
+            'password' => '',
+            'ssh' => '',
+            'keyAPI' => ''
         ],
 
     ],
